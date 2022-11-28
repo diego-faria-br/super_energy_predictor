691abe1287e016125cfce6045a43cac19dbdb59a
alexandre chartier <alexandrechartier@gmail.com>
2022-11-28 12:27:04 -0300

    changes

--- super_energy_predictor/api/fast.py
+++ super_energy_predictor/api/fast.py
@@ -2,9 +2,11 @@ from datetime import datetime
 import pytz
 import pandas as pd
 
+from super_energy_predictor.preprocessing.preprocessing import preprocess
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import lightgbm as lgb
 
 app = FastAPI()
 
@@ -20,14 +22,29 @@ app.add_middleware(
 #uvicorn simple:app --reload
 
 @app.get("/predict")
-def pred(building_id: int, meter: int, timestamp: str):
-    X = {'building_id': [building_id],'meter': [meter],'timestamp': [timestamp]}
+def pred(building_id: int, meter: int, year: str, month: str):
+
+    first_timestamp = "2017-01-01 0:00"
+    last_timestamp = "2017-01-31 23:00"
+
+    dates = pd.date_range(first_timestamp, last_timestamp, freq="1h")
+    dates = pd.DataFrame(dates)
+
+    dates['building_id'] = int(building_id)
+    dates['meter'] = int(meter)
+
+    dates = dates.iloc[:,[1,2,0]]
+
+    #X = {'building_id': [int(building_id)],'meter': [int(meter)], 'period': [int(dates)]}
+
+    X = dates.rename(columns={0:'timestamp'})
     X = preprocess(X)
-    model1 = lgb.Booster(model_file='model1.txt')
-    model2 = lgb.Booster(model_file='model2.txt')
+
+    model1 = lgb.Booster(model_file='raw_data/model1.txt')
+    model2 = lgb.Booster(model_file='raw_data/model2.txt')
     y_pred = (model1.predict(X)+model2.predict(X))/2
 
-    to_return = {'meter_reading':float(y_pred)}
+    to_return = {'energy consumption':float(y_pred)}
 
     return to_return
 
