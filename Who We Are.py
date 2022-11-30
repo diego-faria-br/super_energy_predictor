4602af6f90c5a5e17e4029b00a4d01aa3a677fc0
Diego Faria <diegoliveirafaria@gmail.com>
2022-11-30 21:33:06 +0100

    update

---
+++ Who We Are.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import requests
+import pandas as pd
+import numpy as np
+import datetime
+import plotly.express as ps
+from PIL import Image
+import base64
+
+
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(png_file):
+    with open(png_file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+def build_markup_for_logo(
+    png_file,
+    background_position="50% 10%",
+    margin_top="10%",
+    image_width="60%",
+    image_height="",
+):
+    binary_string = get_base64_of_bin_file(png_file)
+    return """
+            <style>
+                [data-testid="stSidebarNav"] {
+                    background-image: url("data:image/png;base64,%s");
+                    background-repeat: no-repeat;
+                    background-position: %s;
+                    margin-top: %s;
+                    background-size: %s %s;
+                }
+            </style>
+            """ % (
+        binary_string,
+        background_position,
+        margin_top,
+        image_width,
+        image_height,
+    )
+
+
+def add_logo(png_file):
+    logo_markup = build_markup_for_logo(png_file)
+    st.markdown(
+        logo_markup,
+        unsafe_allow_html=True,
+    )
+
+add_logo("Sample.png")
+
+st.markdown("# Home")
+
+
+
+# OR
+
+#st.sidebar.image(add_logo(logo_path="your/logo/path", width=50, height=60))
+
+
+
+st.markdown('# Who We Are?')
+
+st.markdown('# What We Do?')
+
+st.markdown('# How to Use This App?')
